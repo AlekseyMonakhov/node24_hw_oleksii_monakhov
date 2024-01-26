bec668b30231d56c503a78086cdc54c7d5f0872c
Aleksey Monakhov <goshagriboedskii@gmail.com>
2024-01-26 11:25:43 +0200

    upd

--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,8 @@
 function logger(moduleName) {
     return {
-        info: (...reason) => console.log(`${moduleName}: LOG ${reason}`),
-        warn: (...reason) => console.warn(`${moduleName}: WARNING: ${reason}`),
-        error: (...reason) => console.error(`${moduleName}: ERROR: ${reason}`)
+        info: (...reason) => console.log(`${moduleName}: LOG `, ...reason),
+        warn: (...reason) => console.warn(`${moduleName}: WARNING: `, ...reason),
+        error: (...reason) => console.error(`${moduleName}: ERROR: `, ...reason)
     };
 }
 
