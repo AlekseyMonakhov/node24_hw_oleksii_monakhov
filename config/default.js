9031c1a041de94c58b991ae04d0337f767e41ba4
Aleksey Monakhov <goshagriboedskii@gmail.com>
2024-01-28 15:33:35 +0200

    updated default to colorsEnabled to string

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 const logLevel = process.env.LOG_LEVEL || 'warn';
-const colorsEnabled = process.env.COLORS_ENABLED || 0;
+const colorsEnabled = process.env.COLORS_ENABLED || "0";
 
 module.exports = {
     logLevel,
